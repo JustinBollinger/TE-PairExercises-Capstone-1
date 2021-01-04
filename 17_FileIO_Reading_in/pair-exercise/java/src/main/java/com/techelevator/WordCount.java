148134919e027b09298a75b3f969fc4f8d863ed4
Justin Bollinger <jbollingerinterpreter@gmail.com>
2021-01-04 14:48:28 -0500

    Checkpoint Pair Exercises

--- 17_FileIO_Reading_in/pair-exercise/java/src/main/java/com/techelevator/WordCount.java
+++ 17_FileIO_Reading_in/pair-exercise/java/src/main/java/com/techelevator/WordCount.java
@@ -5,10 +5,18 @@ import java.io.File;
 import java.io.FileReader;
 import java.util.Scanner;
 
-public class WordCount {
+public class WordCount
+{
 
-	public static void main(String[] args) {
-
+	public static void main(String[] args)
+	{
+		readAliceInWonderlandTextFile();
+	}
+	
+	public static void readAlicesAdventuresInWonderland()
+	{
+		String filePath = "C:\\Users\\Student\\workspace\\week-4-pair-1\\17_FileIO_Reading_in\\pair-exercise\\java\\alices_adventures_in_wonderland.txt";
+		File alicesAdventures = new File(filePath);
 	}
 
 }
