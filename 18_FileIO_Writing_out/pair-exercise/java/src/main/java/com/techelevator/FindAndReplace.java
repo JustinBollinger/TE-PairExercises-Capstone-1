8c1ac4c2cfa30f1b6c3b84f2ead8320a5cb464bb
Justin Bollinger <jbollingerinterpreter@gmail.com>
2021-01-05 13:54:50 -0500

    Checkpoint File IO Pt 2 Pair Exercises -JB

--- 18_FileIO_Writing_out/pair-exercise/java/src/main/java/com/techelevator/FindAndReplace.java
+++ 18_FileIO_Writing_out/pair-exercise/java/src/main/java/com/techelevator/FindAndReplace.java
@@ -1,9 +1,58 @@
 package com.techelevator;
 
-public class FindAndReplace {
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.PrintStream;
+import java.util.Scanner;
 
-	public static void main(String[] args) {
+import javax.print.event.PrintServiceAttributeEvent;
 
+public class FindAndReplace
+{
+
+	public static void main(String[] args)
+	{
+		Scanner userInput = new Scanner (System.in);
+		System.out.print("Enter file name: ");
+		String path = userInput.nextLine();
+		
+		File aliceText = new File(path);
+		
+		System.out.println(aliceText);
+		System.out.println(aliceText.getAbsolutePath());
+		
+		// logic 
+		
+		
+		
+		if(aliceText.exists())
+		{
+			aliceText.delete();
+			System.out.println("deleting file");
+		}
+		
+		try
+		{
+			System.out.print("Enter word to search for: ");
+			String enteredWord = userInput.nextLine();
+			
+			System.out.println("Enter replacement word: ");
+			String replacementWord = userInput.nextLine();
+			
+			
+			FileOutputStream fileStream = new FileOutputStream(aliceText, true);
+			PrintStream fileWriter = new PrintStream(fileStream);
+			
+			fileWriter.println(enteredWord);
+			fileWriter.close();
+
+			
+			
+			
+		} catch (Exception e)
+		{
+			// TODO: handle exception
+		}
 	}
 
 }
